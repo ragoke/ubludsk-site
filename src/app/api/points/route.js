3f7e8c9040448558bb93f6e519b9c648bc4092d4
Ragoke <sazonovdaniil123@gmail.com>
2024-08-01 04:14:16 +0300

    1.0

--- src/app/api/points/route.js
+++ src/app/api/points/route.js
@@ -3,28 +3,20 @@ import Point from "@/models/point";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-	try {
-		await connectMongoDB();
-		const points = await Point.find();
-		return NextResponse.json(points);
-	} catch (error) {
-		return NextResponse.json({ error: "Not Found" }, { status: 404 });
-	}
+	await connectMongoDB();
+	const points = await Point.find() || [];
+	return NextResponse.json(points);
 }
 
 export async function PATCH(req) {
-	try {
-		const { sprite, value } = await req.json();
-		console.log({ sprite, value });
-		return NextResponse.json({});
-	} catch (error) {
-		return NextResponse.json({ error: "Bad Request" }, { status: 400 });
-	}
+	const { sprite, value } = await req.json();
+	console.log({ sprite, value });
+	return NextResponse.json({});
 }
 
 export async function POST(req) {
 	const { player, sprite, value } = await req.json();
 	await connectMongoDB();
 	await Point.findOneAndUpdate({ sprite }, { value: parseInt(value) });
-	return NextResponse.json({ message: "Point value changed" }, { status: 200 });
+	return NextResponse.json({});
 }

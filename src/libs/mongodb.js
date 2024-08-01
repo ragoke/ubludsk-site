c2ab9baa025309704a95ae1bcc07ddae7f228c7a
Ragoke <67712287+ragoke@users.noreply.github.com>
2024-08-01 03:47:19 +0300

    1.0

--- src/libs/mongodb.js
+++ src/libs/mongodb.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const connectMongoDB = async () => {
 	try {
 		await mongoose.connect(process.env.MONGODB_URI);
+		console.log('connected to database');
 	} catch (error) {
 		console.log(error);
 	}
